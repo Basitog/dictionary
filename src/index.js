d63c7ed00ad67be1872d4cbe7f635d086058b0e8
Abdul Basit <salmonbasit@gmail.com>
2023-05-12 02:02:14 +0100

    add audio

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     $('#weatherLocation').click(function () {
         const word = $('#location').val();
         $('#location').val("");
+        
 
         let request = new XMLHttpRequest();
         const url = `http://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
@@ -26,6 +27,12 @@ $(document).ready(function () {
             $('.definition2').text(`The second definition of ${word} is ${response[0].meanings[0].definitions[1].definition}`);
             $('.synonyms').text(`The synonyms of ${word} is ${response[0].meanings[1].definitions[0].definition}`);
             $('.Antonyms').text(`The antonyms of ${word} is ${response[0].meanings[0].definitions[0].definition}`);
+            $('.Example1').text(`The example of ${word} is ${response[0].meanings[0].definitions[0].definition}`);
+            $('.Example2').text(`The second example of ${word} is ${response[0].meanings[0].definitions[1].definition}`);
+            $('.partofspeech').text(`The part of speech of ${word} is ${response[0].meanings[1].partOfSpeech}`);
+            // $('.audio').html(`The audio of ${word} is ${response[0].phonetics[0].audio }`)
+            $('.Audio').html(`The audio of ${word} is <audio controls><source src="${response[0].phonetics[0].audio}" type="audio/mpeg"></audio>`);
+
         }
     });
 });
